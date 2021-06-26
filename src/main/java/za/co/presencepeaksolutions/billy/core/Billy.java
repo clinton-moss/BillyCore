a5c182c6b9d220bff882bf12e05173347bca4cc9
clinton <clinton.moss@google.com>
2021-06-26 10:54:49 +0200

    Init Billy

--- src/main/java/za/co/presencepeaksolutions/billy/core/Billy.java
+++ src/main/java/za/co/presencepeaksolutions/billy/core/Billy.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.ComponentScan;
 import za.co.presencepeaksolutions.billy.core.users.User;
 
 /**
- *
+ * 
  * @author clinton
  */
 @SpringBootApplication
