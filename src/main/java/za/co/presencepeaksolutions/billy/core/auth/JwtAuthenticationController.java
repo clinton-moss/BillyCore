aa178a6ec0fa43441ecfdfaae7a464e949c85242
clinton <clinton.moss@google.com>
2021-06-29 09:49:17 +0200

    Adding JWS Authentication

--- src/main/java/za/co/presencepeaksolutions/billy/core/auth/JwtAuthenticationController.java
+++ src/main/java/za/co/presencepeaksolutions/billy/core/auth/JwtAuthenticationController.java
@@ -5,7 +5,9 @@
  */
 package za.co.presencepeaksolutions.billy.core.auth;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +16,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +46,13 @@ public class JwtAuthenticationController {
     private JwtUserDetailsService userDetailsService;
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest,
+            @RequestHeader HttpHeaders headers,
+            HttpServletRequest request) throws Exception {
+
+        if (headers.containsKey("DeviceType")) {
+            // used for different auth methods accorss platforms
+        }
 
         authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
